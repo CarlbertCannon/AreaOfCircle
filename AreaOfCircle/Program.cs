e3528de90ccd3395755960b43dc06bbd921ac38b
CarlbertCannon <providenceneedschange@gmail.com>
2021-07-10 14:58:25 -0600

    intial commit

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = 20;
+            var pi = Math.PI;
+
+            var areaOfCircle = pi * (r * r);
+
+            Console.WriteLine($"The area of a circle with the radius of {r} is {areaOfCircle}");
+        }
+    }
+}
